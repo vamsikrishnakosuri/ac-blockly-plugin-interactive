243abd399943062d24daa6846113ee00aa469fb1
Rubel Hassan Mollik <rubelhassan@outlook.com>
2025-05-29 10:50:49 +0600

    Initialize accessible blockly plugin codebase

---
+++ src/index.js
@@ -0,0 +1,22 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as Constants from '../src/constants';
+
+import {
+  FlyoutCursor,
+  pluginInfo as FlyoutCursorPluginInfo,
+} from './cursors/flyout_cursor';
+import {Navigation} from './navigation';
+import {NavigationController} from './navigation_controller';
+
+export {
+  Constants,
+  FlyoutCursor,
+  FlyoutCursorPluginInfo,
+  Navigation,
+  NavigationController
+};
